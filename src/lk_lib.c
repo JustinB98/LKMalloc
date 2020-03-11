9afe96cf34d7feeef3813ce1d93b180fed4b1fa3
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-11 18:00:19 -0400

    Got a working implementation of lkmalloc Works on basic mallocs. Need to finish lkfree to test more

--- src/lk_lib.c
+++ src/lk_lib.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
 #include "lk_lib.h"
 #include "lk_data.h"
@@ -36,15 +37,24 @@ static void *get_ptr(u_int size, u_int flags) {
 	u_int new_size = get_actual_size(size, flags);
 	void *malloced_ptr = malloc(new_size);
 	if (malloced_ptr == NULL) return NULL;
+	if (flags & LKM_INIT) memset(malloced_ptr, 0, size);
 	fill_around_malloced_ptr(malloced_ptr, size, flags);
 	return malloced_ptr;
 }
 
-int lkmalloc(u_int size, void **ptr, u_int flags) {
+static void *get_user_ptr(void *malloced_ptr, u_int flags) {
+	if (malloced_ptr == NULL) return NULL;
+	void *user_ptr = malloced_ptr;
+	if (flags & LKM_UNDER) user_ptr = malloced_ptr + 8;
+	return user_ptr;
+}
+
+int __lkmalloc_internal(u_int size, void **ptr, u_int flags, char *file, const char *func, int line) {
 	init_if_needed();
 	void *malloced_ptr = get_ptr(size, flags);
 	if (malloced_ptr == NULL) return -errno;
-	lk_data_insert(malloced_ptr, size, flags);
-	*ptr = malloced_ptr;
+	lk_data_insert(malloced_ptr, size, flags, file, func, line);
+	*ptr = get_user_ptr(malloced_ptr, flags);
 	return 0;
 }
+
