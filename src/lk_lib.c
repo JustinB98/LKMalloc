22fb1f4be0cd3e14268a11247591706f57228523
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-12 00:54:20 -0400

    Implement lkfree Works for very basic tests

--- src/lk_lib.c
+++ src/lk_lib.c
@@ -58,3 +58,48 @@ int __lkmalloc_internal(u_int size, void **ptr, u_int flags, char *file, const c
 	return 0;
 }
 
+static void terminate_program_if_error(u_int flags) {
+	if (flags & LKF_ERROR) {
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void data_not_found(u_int flags, char *file, const char *func, int line) {
+	if (flags & LKF_UNKNOWN) {
+		fprintf(stderr, "Trying to allocate a pointer that was never lkmalloced\n");
+	}
+	terminate_program_if_error(flags);
+}
+
+static int already_free(LK_DATA *data_found, u_int flags) {
+	fprintf(stderr, "%p was already freed\n", data_found->ptr);
+	return 0;
+}
+
+static int attempt_to_free(LK_DATA *data_found, u_int flags, void *original_ptr) {
+	int was_freed = data_found->freed != 0;
+	++(data_found->freed);
+	if (was_freed) {
+		already_free(data_found, flags);
+		return -EINVAL;
+	}
+	/* If pointers are different, then LKF_APPROX was part of flags */
+	if (data_found->ptr != original_ptr && (flags & LKF_WARN)) {
+		fprintf(stderr, "WARNING - Freeing a pointer using LKF_APPROX\n");
+		terminate_program_if_error(flags);
+	}
+	void *ptr_to_free = data_found->ptr;
+	if (data_found->flags & LKM_OVER) ptr_to_free -= 8;
+	free(ptr_to_free);
+	return 0;
+}
+
+int __lkfree_internal(void **ptr, u_int flags, char *file, const char *func, int line) {
+	init_if_needed();
+	LK_DATA *data_found = lk_data_find(*ptr, flags);
+	if (data_found == NULL) {
+		data_not_found(flags, file, func, line);
+		return -EINVAL;
+	}
+	return attempt_to_free(data_found, flags, *ptr);
+}
