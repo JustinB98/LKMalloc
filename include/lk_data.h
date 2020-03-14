fa58d500a93937a1353046b935cc32f14a22a3ec
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-14 18:46:08 -0400

    Reworked the structure and implementation of lk_data module Changed the definition of the struct holding the data. Malloced records and free records can now be stored. Right now they're stored in the same data structure, but will likely change in the future.
    Also changed around some implementations to fit around this new struct

--- include/lk_data.h
+++ include/lk_data.h
@@ -1,19 +1,37 @@
 #ifndef LK_DATA_H
 #define LK_DATA_H
 
-typedef struct lk_data {
-	void *ptr;
-	char *file;
-	const char *func;
-	u_int size, flags;
-	int freed, line;
-} LK_DATA;
+#include <sys/time.h>
+
+typedef struct lk_record {
+	int record_type, line_num, retval;
+	u_int flags;
+	char *file_name;
+	const char *function_name;
+	void **ptr_passed;
+	time_t seconds;
+	suseconds_t microseconds;
+	void *sub_record; /* Used for either lk_malloc_record or lk_free_record */
+} LK_RECORD;
+
+typedef struct lk_malloc_record {
+	int times_freed;
+	u_int size;
+	void *addr_returned;
+	void *malloced_ptr;
+} LK_MALLOC_RECORD;
+
+typedef struct lk_free_record {
+	void *ptr_requested;
+	void *ptr_freed; /* NULL if no ptr was freed */
+} LK_FREE_RECORD;
 
 void lk_data_init();
 
-void lk_data_insert(void *ptr, u_int size, u_int flags, char *file, const char *func, int line);
+LK_RECORD *lk_data_insert_malloc_record(void **ptr_passed, u_int size, u_int flags, char *file, const char *function, int line);
+LK_RECORD *lk_data_insert_free_record(void **ptr_passed, void *ptr_freed, u_int flags, char *file, const char *func, int line);
 
-LK_DATA *lk_data_find(void *ptr, u_int flags);
+LK_RECORD *lk_data_find_malloc_record(LK_FREE_RECORD *free_record, void *ptr, u_int flags);
 
 void lk_data_remove(void *ptr);
 
--- src/lk_data.c
+++ src/lk_data.c
@@ -11,43 +11,77 @@ static void *current_ptr_to_find;
 static u_int global_flags;
 
 static int lk_finder(void *data) {
-	LK_DATA *lk_data = data;
-	if (lk_data->ptr == current_ptr_to_find) return 1;
+	LK_RECORD *record = data;
+	if (record->record_type != 0) return 0;
+	LK_MALLOC_RECORD *malloc_record = record->sub_record;
+	if (malloc_record->addr_returned == current_ptr_to_find) return 1;
 	else if ((global_flags & LKF_APPROX) == 0) return 0;
-	u_int ptr_size = lk_data->size;
-	void *start_ptr = lk_data->ptr;
+	u_int ptr_size = malloc_record->size;
+	void *start_ptr = malloc_record->addr_returned;
 	void *end_ptr = start_ptr + ptr_size;
 	return start_ptr < current_ptr_to_find && current_ptr_to_find < end_ptr;
 }
 
-static void onRemoval(void *data) {
-	LK_DATA *lk_data = data;
-	if (lk_data->freed == 0) {
-		free(lk_data->ptr);
+static void free_malloc_record(LK_MALLOC_RECORD *record) {
+	if (record->times_freed == 0) {
+		free(record->malloced_ptr);
 	}
-	free(lk_data);
+}
+
+static void onRemoval(void *data) {
+	LK_RECORD *record = data;
+	if (record->record_type == 0) free_malloc_record(record->sub_record);
+	free(record->sub_record);
+
+	free(record);
 }
 
 void lk_data_init() {
 	list = linked_list_init(onRemoval);	
 }
 
-void lk_data_insert(void *ptr, u_int size, u_int flags, char *file, const char *func, int line) {
-	LK_DATA *lk_data = malloc(sizeof(LK_DATA));
-	lk_data->ptr = ptr;
-	lk_data->size = size;
-	lk_data->flags = flags;
-	lk_data->freed = 0;
-	lk_data->file = file;
-	lk_data->func = func;
-	lk_data->line = line;
-	linked_list_insert(list, ptr, lk_data);
+static LK_RECORD *lk_data_insert(int record_type, int line_num, u_int flags, char *file, const char *function_name, void **ptr_passed, void *sub_record) {
+	LK_RECORD *rec = malloc(sizeof(LK_RECORD));
+	rec->record_type = record_type;
+	rec->line_num = line_num;
+	rec->flags = flags;
+	rec->file_name = file;
+	rec->function_name = function_name;
+	rec->ptr_passed = ptr_passed;
+	rec->sub_record = sub_record;
+	struct timeval tv;
+	int ret = gettimeofday(&tv, NULL);
+	if (ret < 0) {
+		perror("gettimeofday");
+		return NULL;
+	}
+	rec->seconds = tv.tv_sec;
+	rec->microseconds = tv.tv_usec;
+	linked_list_insert(list, NULL, rec);
+	return rec;
 }
 
-LK_DATA *lk_data_find(void *ptr, u_int flags) {
+LK_RECORD *lk_data_insert_malloc_record(void **ptr_passed, u_int size, u_int flags, char *file, const char *function, int line) {
+	LK_MALLOC_RECORD *malloc_record = malloc(sizeof(LK_MALLOC_RECORD));
+	malloc_record->times_freed = 0;
+	malloc_record->size = size;
+	return lk_data_insert(0, line, flags, file, function, ptr_passed, malloc_record);
+}
+
+LK_RECORD *lk_data_insert_free_record(void **ptr_passed, void *ptr_requested, u_int flags, char *file, const char *function, int line) {
+	LK_FREE_RECORD *free_record = malloc(sizeof(LK_FREE_RECORD));
+	free_record->ptr_requested = ptr_requested;
+	return lk_data_insert(1, line, flags, file, function, ptr_passed, free_record);
+}
+
+LK_RECORD *lk_data_find_malloc_record(LK_FREE_RECORD *free_record, void *ptr, u_int flags) {
 	current_ptr_to_find = ptr;
 	global_flags = flags;
-	LK_DATA *result = linked_list_find(list, lk_finder);
+	LK_RECORD *result = linked_list_find(list, lk_finder);
+	if (result != NULL) {
+		LK_MALLOC_RECORD *malloc_record = result->sub_record;
+		free_record->ptr_freed = malloc_record->addr_returned;
+	}
 	current_ptr_to_find = NULL;
 	global_flags = 0;
 	return result;
