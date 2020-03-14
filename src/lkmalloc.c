56a68717ae542265bb80fc71a80212140214b253
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-14 18:47:51 -0400

    Reworked lkmalloc.c to fit lk_data module reworking The code is a bit messy but right now it's just about getting the program to work, then neating up later

--- src/lkmalloc.c
+++ src/lkmalloc.c
@@ -51,10 +51,17 @@ static void *get_user_ptr(void *malloced_ptr, u_int flags) {
 
 int __lkmalloc_internal(u_int size, void **ptr, u_int flags, char *file, const char *func, int line) {
 	init_if_needed();
+	LK_RECORD *record = lk_data_insert_malloc_record(ptr, size, flags, file, func, line);
+	LK_MALLOC_RECORD *mal_rec = record->sub_record;
 	void *malloced_ptr = get_ptr(size, flags);
-	if (malloced_ptr == NULL) return -errno;
-	lk_data_insert(malloced_ptr, size, flags, file, func, line);
+	mal_rec->malloced_ptr = malloced_ptr;
+	if (malloced_ptr == NULL) { 
+		record->retval = -errno;
+		return -errno;
+	}
 	*ptr = get_user_ptr(malloced_ptr, flags);
+	mal_rec->addr_returned = *ptr;
+	record->retval = 0;
 	return 0;
 }
 
@@ -71,36 +78,43 @@ static void data_not_found(u_int flags, char *file, const char *func, int line) 
 	terminate_program_if_error(flags);
 }
 
-static int already_free(LK_DATA *data_found, u_int flags) {
-	fprintf(stderr, "%p was already freed\n", data_found->ptr);
+static int already_free(void *ptr, u_int flags) {
+	fprintf(stderr, "%p was already freed\n", ptr);
 	return 0;
 }
 
-static int attempt_to_free(LK_DATA *data_found, u_int flags, void *original_ptr) {
-	int was_freed = data_found->freed != 0;
-	++(data_found->freed);
+static int attempt_to_free(LK_RECORD *data_found, u_int flags, void *original_ptr) {
+	LK_MALLOC_RECORD *mal_rec = data_found->sub_record;
+	int was_freed = mal_rec->times_freed != 0;
+	++(mal_rec->times_freed);
 	if (was_freed) {
-		already_free(data_found, flags);
+		already_free(mal_rec->addr_returned, flags);
 		return -EINVAL;
 	}
 	/* If pointers are different, then LKF_APPROX was part of flags */
-	if (data_found->ptr != original_ptr && (flags & LKF_WARN)) {
+	if (mal_rec->addr_returned != original_ptr && (flags & LKF_WARN)) {
 		fprintf(stderr, "WARNING - Freeing a pointer using LKF_APPROX\n");
 		terminate_program_if_error(flags);
 	}
-	void *ptr_to_free = data_found->ptr;
-	if (data_found->flags & LKM_OVER) ptr_to_free -= 8;
-	free(ptr_to_free);
+	free(mal_rec->malloced_ptr);
 	return 0;
 }
 
 int __lkfree_internal(void **ptr, u_int flags, char *file, const char *func, int line) {
 	init_if_needed();
-	LK_DATA *data_found = lk_data_find(*ptr, flags);
+	LK_RECORD *free_record = lk_data_insert_free_record(ptr, *ptr, flags, file, func, line);
+	LK_RECORD *data_found = lk_data_find_malloc_record(free_record->sub_record, *ptr, flags);
 	if (data_found == NULL) {
 		data_not_found(flags, file, func, line);
+		free_record->retval = -EINVAL;
 		return -EINVAL;
 	}
-	return attempt_to_free(data_found, flags, *ptr);
+	free_record->retval = attempt_to_free(data_found, flags, *ptr);
+	return free_record->retval;
+}
+
+int __lk_report_internal(int fd, u_int flags) {
+	
+	return 0;
 }
 
