14e809ee398615bad8886c22efd70b9620621105
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-22 16:57:35 -0400

    Changed definition of a record Changed record to have more information and to require less malloc calls than the previous (which used two malloced pointers, this one uses only one, for the struct itself). Used a union for malloc and free records to save space
    One weakness of this new record struct is it's a very long path to get to a malloc or free record, for example, for a free record:
    
    record->sub_record.free_record
    
    This promotes ugly code. Will fix at a later point if a better way is thought of
    
    Also changed the methods to take in less arguments. This requires the caller (lkmalloc.c) to create an LK_RECORD, but also balances the workload

--- include/lk_data.h
+++ include/lk_data.h
@@ -3,42 +3,51 @@
 
 #include <sys/time.h>
 
-typedef struct lk_record {
-	int record_type, line_num, retval;
-	u_int flags;
+typedef struct lk_metadata {
+	int line_num;
 	char *file_name;
 	const char *function_name;
-	void **ptr_passed;
-	time_t seconds;
-	suseconds_t microseconds;
-	void *sub_record; /* Used for either lk_malloc_record or lk_free_record */
-} LK_RECORD;
+	struct timeval timestamp;
+} LK_METADATA;
 
-typedef struct lk_malloc_record {
+typedef struct malloc_record {
 	int times_freed;
 	u_int size;
 	void *addr_returned;
 	void *malloced_ptr;
-} LK_MALLOC_RECORD;
+} MALLOC_RECORD;
 
-typedef struct lk_free_record {
+typedef struct free_record {
 	void *ptr_requested;
-	void *ptr_freed; /* NULL if no ptr was freed */
-} LK_FREE_RECORD;
+	void *ptr_freed;
+	int n_time_free, orphaned_free;
+} FREE_RECORD;
+
+typedef struct lk_record {
+	LK_METADATA metadata;
+	int record_type, retval;
+	void **ptr_passed;
+	u_int flags;
+	union {
+		MALLOC_RECORD malloc_record;
+		FREE_RECORD free_record;
+	} sub_record;
+} LK_RECORD;
 
 void lk_data_init();
 
-LK_RECORD *lk_data_insert_malloc_record(void **ptr_passed, u_int size, u_int flags, char *file, const char *function, int line);
-LK_RECORD *lk_data_insert_free_record(void **ptr_passed, void *ptr_freed, u_int flags, char *file, const char *func, int line);
-
-LK_RECORD *lk_data_find_malloc_record(LK_FREE_RECORD *free_record, void *ptr, u_int flags);
-
-void lk_data_remove(void *ptr);
+void lk_data_insert_failed_record(LK_RECORD *record);
 
 /**
- * Functions should return negative number on failure, 0 or positive number on success
+ * Will insert malloc record into pending list
  */
-int lk_data_for_each(int (*mal_consumer)(LK_RECORD *), int (*free_consumer)(LK_RECORD *));
+void lk_data_insert_malloc_record(void *ptr, LK_RECORD *malloc_record);
+
+/**
+ * Will attempt to look for a malloc record in the active heap
+ * If it can't, then the completed records need to be searched
+ */
+void *lk_data_insert_free_record(void *ptr, LK_RECORD *free_record, int (*finder)(void *, void *));
 
 void lk_data_fini();
 
