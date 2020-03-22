e1eb1863cf8580e6992657cad82683e34128796f
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-22 17:01:41 -0400

    Remade lk_data.c to fix the new record structure The result is a lot better looking code with less global variables. The code hopefully will perform better at the cost of more memory

--- src/lk_data.c
+++ src/lk_data.c
@@ -3,119 +3,87 @@
 
 #include "lk_data.h"
 #include "linked_list.h"
-#include "lkmalloc.h"
+#include "binary_tree.h"
 
-static LINKED_LIST *list;
+static LINKED_LIST *all_records_list;
+static BINARY_TREE *completed_records;
+static BINARY_TREE *active_records;
 
-static void *current_ptr_to_find;
-static u_int global_flags;
-static int (*m_consumer)(LK_RECORD *);
-static int (*f_consumer)(LK_RECORD *);
-
-static int lk_finder(void *data) {
-	LK_RECORD *record = data;
-	if (record->record_type != 0) return 0;
-	LK_MALLOC_RECORD *malloc_record = record->sub_record;
-	if (malloc_record->addr_returned == current_ptr_to_find) return 1;
-	else if ((global_flags & LKF_APPROX) == 0) return 0;
-	u_int ptr_size = malloc_record->size;
-	void *start_ptr = malloc_record->addr_returned;
-	void *end_ptr = start_ptr + ptr_size;
-	return start_ptr < current_ptr_to_find && current_ptr_to_find < end_ptr;
+void lk_data_init() {
+	all_records_list = linked_list_init();
+	completed_records = binary_tree_init();
+	active_records = binary_tree_init();
 }
 
-static void free_malloc_record(LK_MALLOC_RECORD *record) {
-	if (record->times_freed == 0) {
-		// free(record->malloced_ptr);
-		fprintf(stderr, "WARNING - Memory leak! %p was never freed!\n", record->addr_returned);
+void lk_data_insert_malloc_record(void *ptr, LK_RECORD *malloc_record) {
+	linked_list_insert(all_records_list, malloc_record);
+	binary_tree_insert(active_records, ptr, malloc_record);
+}
+
+void lk_data_insert_failed_record(LK_RECORD *record) {
+	linked_list_insert(all_records_list, record);
+}
+
+static void *mark_as_complete(LK_RECORD *free_record, LK_RECORD *mal_record) {
+	void *malloced_ptr = mal_record->sub_record.malloc_record.malloced_ptr;
+	free_record->sub_record.free_record.ptr_freed = malloced_ptr;
+	++(mal_record->sub_record.malloc_record.times_freed);
+	free_record->sub_record.free_record.n_time_free = mal_record->sub_record.malloc_record.times_freed;
+	binary_tree_remove(active_records, mal_record);
+	binary_tree_replace(completed_records, malloced_ptr, mal_record);
+	return malloced_ptr;
+}
+
+static void mark_as_double_free(LK_RECORD *free_record, LK_RECORD *malloc_record) {
+	int times_freed = malloc_record->sub_record.malloc_record.times_freed + 1;
+	malloc_record->sub_record.malloc_record.times_freed = times_freed;
+	free_record->sub_record.free_record.n_time_free = times_freed;
+	free_record->sub_record.free_record.orphaned_free = 0;
+}
+
+static void mark_as_orphaned_free(LK_RECORD *free_record) {
+	free_record->sub_record.free_record.n_time_free = 0;
+	free_record->sub_record.free_record.orphaned_free = 1;
+}
+
+void *lk_data_insert_free_record(void *ptr, LK_RECORD *free_record, int (*finder)(void *, void *)) {
+	linked_list_insert(all_records_list, free_record);
+	LK_RECORD *mal_record = binary_tree_find_with_function(active_records, ptr, free_record, finder);
+	if (mal_record != NULL) {
+		free_record->sub_record.free_record.n_time_free = 1;
+		free_record->sub_record.free_record.orphaned_free = 0;
+		return mark_as_complete(free_record, mal_record);
 	}
+	mal_record = binary_tree_find_with_function(completed_records, ptr, free_record, finder);
+	if (mal_record != NULL) {
+		mark_as_double_free(free_record, mal_record);
+		return NULL;
+	}
+	mark_as_orphaned_free(free_record);
+	return NULL;
+}
+
+static void free_malloc_record(LK_RECORD *malloc_record) {
+	free(malloc_record);
+}
+
+static void free_free_record(LK_RECORD *free_record) {
+	free(free_record);
 }
 
 static void onRemoval(void *data) {
 	LK_RECORD *record = data;
-	if (record->record_type == 0) free_malloc_record(record->sub_record);
-	free(record->sub_record);
-
-	free(record);
-}
-
-void lk_data_init() {
-	list = linked_list_init(onRemoval);	
-}
-
-static LK_RECORD *lk_data_insert(int record_type, int line_num, u_int flags, char *file, const char *function_name, void **ptr_passed, void *sub_record) {
-	LK_RECORD *rec = malloc(sizeof(LK_RECORD));
-	rec->record_type = record_type;
-	rec->line_num = line_num;
-	rec->flags = flags;
-	rec->file_name = file;
-	rec->function_name = function_name;
-	rec->ptr_passed = ptr_passed;
-	rec->sub_record = sub_record;
-	struct timeval tv;
-	int ret = gettimeofday(&tv, NULL);
-	if (ret < 0) {
-		perror("gettimeofday");
-		return NULL;
-	}
-	rec->seconds = tv.tv_sec;
-	rec->microseconds = tv.tv_usec;
-	linked_list_insert(list, NULL, rec);
-	return rec;
-}
-
-LK_RECORD *lk_data_insert_malloc_record(void **ptr_passed, u_int size, u_int flags, char *file, const char *function, int line) {
-	LK_MALLOC_RECORD *malloc_record = malloc(sizeof(LK_MALLOC_RECORD));
-	malloc_record->times_freed = 0;
-	malloc_record->size = size;
-	return lk_data_insert(0, line, flags, file, function, ptr_passed, malloc_record);
-}
-
-LK_RECORD *lk_data_insert_free_record(void **ptr_passed, void *ptr_requested, u_int flags, char *file, const char *function, int line) {
-	LK_FREE_RECORD *free_record = malloc(sizeof(LK_FREE_RECORD));
-	free_record->ptr_requested = ptr_requested;
-	free_record->ptr_freed = NULL;
-	return lk_data_insert(1, line, flags, file, function, ptr_passed, free_record);
-}
-
-LK_RECORD *lk_data_find_malloc_record(LK_FREE_RECORD *free_record, void *ptr, u_int flags) {
-	current_ptr_to_find = ptr;
-	global_flags = flags;
-	LK_RECORD *result = linked_list_find(list, lk_finder);
-	if (result != NULL) {
-		LK_MALLOC_RECORD *malloc_record = result->sub_record;
-		free_record->ptr_freed = malloc_record->addr_returned;
-	}
-	current_ptr_to_find = NULL;
-	global_flags = 0;
-	return result;
-}
-
-void lk_data_remove(void *ptr) {
-	linked_list_remove(list, ptr);
-}
-
-static int lk_consumer(void *data) {
-	LK_RECORD *rec = data;
-	int ret = 0;
-	if (rec->record_type == 0) {
-		ret = m_consumer(rec);
+	if (record->record_type == 0) {
+		free_malloc_record(record);
+	} else if (record->record_type == 1) {
+		free_free_record(record);
 	} else {
-		ret = f_consumer(rec);
+		fprintf(stderr, "WARNING - Unknown record trying to be freed");
 	}
-	return ret < 0 ? 1 : 0;
-}
-
-int lk_data_for_each(int (*mal_consumer)(LK_RECORD *), int (*free_consumer)(LK_RECORD *)) {
-	m_consumer = mal_consumer;
-	f_consumer = free_consumer;
-	/* Taking advantage that we can pass a function into the finder */
-	void *returned = linked_list_find(list, lk_consumer);
-	// NULL means entire process completed
-	// Non null means the process was interuptted by a failed read
-	return returned == NULL ? 0 : -1;
 }
 
 void lk_data_fini() {
-	linked_list_fini(list);
+	binary_tree_fini(active_records, NULL);
+	binary_tree_fini(completed_records, NULL);
+	linked_list_fini(all_records_list, onRemoval);
 }
