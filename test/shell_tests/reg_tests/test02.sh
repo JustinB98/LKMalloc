6a3f426db4766252cbd44c8ef9f1cfe7c4128064
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-27 19:55:33 -0400

    Added test02.sh which tests lkfree

---
+++ test/shell_tests/reg_tests/test02.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+total_pass=0
+total_fail=0
+
+assert_success=". shell_tests/assert_successful_exit.sh"
+assert_fail=". shell_tests/assert_unsuccessful_exit.sh"
+
+# Double free takes care of everything. Just have to make sure it exists successfully
+double_free="bin/double_free_test"
+
+echo "============ TEST02.SH START ==============="
+
+test_name="Double Free 1"
+$double_free 1 > /dev/null 2>&1
+$assert_success
+
+test_name="Double Free 10"
+$double_free 10 > /dev/null 2>&1
+$assert_success
+
+test_name="Double Free 1000"
+$double_free 1000 > /dev/null 2>&1
+$assert_success
+
+printf "==================== TEST02.SH END ====================\n"
+test_type="test01.sh"
+. shell_tests/finish.sh
