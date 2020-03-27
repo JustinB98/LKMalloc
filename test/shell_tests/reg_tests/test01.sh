38072e0a4da4263b53c071c390612bcea2e3450f
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-27 19:50:22 -0400

    Fixed huge test bug Turns out the way to output to /dev/null was not the way it was originally done. Changed it so it's a little more verbose but it works
    Also the test_failed script location was spelled wrong

--- test/shell_tests/reg_tests/test01.sh
+++ test/shell_tests/reg_tests/test01.sh
@@ -4,7 +4,7 @@ total_pass=0
 total_fail=0
 
 assert_success=". shell_tests/assert_successful_exit.sh"
-assert_fail=". shell_tests./assert_unsuccesful_exit.sh"
+assert_fail=". shell_tests/assert_unsuccessful_exit.sh"
 
 stress_test="bin/stress_test"
 assert_block="bin/assert_memory_block"
@@ -12,39 +12,39 @@ assert_block="bin/assert_memory_block"
 echo "============ TEST01.SH START ==============="
 
 test_name="Stress test 100"
-$stress_test 100 2&1> /dev/null
+$stress_test 100 > /dev/null 2>&1
+$assert_success
+
+test_name="Stress test 100"
+$stress_test 100 > /dev/null 2>&1
 $assert_success
 
 test_name="Stress test 1000"
-$stress_test 1000 2&1> /dev/null
-$assert_success
-
-test_name="Stress test 10000"
-$stress_test 10000 2&1> /dev/null
+$stress_test 1000 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_INIT test 10 bytes"
-$assert_block 10 0x1 0 0 10 2&1> /dev/null
+$assert_block 10 0x1 0 0 10 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_INIT test 100 bytes"
-$assert_block 100 0x1 0 0 100 2&1> /dev/null
+$assert_block 100 0x1 0 0 100> /dev/null 2>&1
 $assert_success
 
 test_name="LKM_UNDER test"
-$assert_block 10 0x4 -8 0x6b 8 2&1> /dev/null
+$assert_block 10 0x4 -8 0x6b 8 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_OVER test"
-$assert_block 10 0x2 18 0x5a 8 2&1> /dev/null
+$assert_block 10 0x2 10 0x5a 8 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_UNDER and LKM_OVER check UNDER test"
-$assert_block 10 0x6 -8 0x6a 8 2&1> /dev/null
+$assert_block 10 0x6 -8 0x6b 8 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_UNDER and LKM_OVER check OVER test"
-$assert_block 10 0x6 18 0x6a 8 2&1> /dev/null
+$assert_block 10 0x6 10 0x5a 8 > /dev/null 2>&1
 $assert_success
 
 printf "==================== TEST01.SH END ====================\n"
