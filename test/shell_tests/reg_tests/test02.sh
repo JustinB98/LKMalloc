c6fa7fa3fad7e04c26f2682977321d9a18e895f6
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-27 21:36:34 -0400

    Added more lkfree tests

--- test/shell_tests/reg_tests/test02.sh
+++ test/shell_tests/reg_tests/test02.sh
@@ -8,6 +8,7 @@ assert_fail=". shell_tests/assert_unsuccessful_exit.sh"
 
 # Double free takes care of everything. Just have to make sure it exists successfully
 double_free="bin/double_free_test"
+shift_and_free="bin/shift_and_free"
 
 echo "============ TEST02.SH START ==============="
 
@@ -23,6 +24,39 @@ test_name="Double Free 1000"
 $double_free 1000 > /dev/null 2>&1
 $assert_success
 
+test_name="Free With LKF_APPROX"
+$shift_and_free 10 0 0x3 5 2> test_file > /dev/null
+$assert_success
+
+test_name="Print with Warning Test"
+grep "WARNING" test_file > /dev/null 2>&1
+$assert_success
+
+test_name="Freeing With LKF_APPROX redzone under"
+$shift_and_free 10 0x4 0x1 -8 2> test_file > /dev/null
+$assert_success
+
+test_name="Freeing With LKF_APPROX under redzone under printed warning"
+grep "WARNING" test_file > /dev/null 2>&1
+$assert_success
+
+test_name="Freeing With LKF_APPROX redzone over"
+$shift_and_free 10 0x2 0x1 17 2> test_file > /dev/null
+$assert_success
+
+test_name="Freeing With LKF_APPROX under redzone over printed warning"
+grep "WARNING" test_file > /dev/null 2>&1
+$assert_success
+
+test_name="lkmalloc exits with LKF_APPROX and LKF_ERROR"
+$shift_and_free 10 0 0xb 5 2> test_file > /dev/null
+$assert_fail
+
+test_name="lkmalloc printed a message with LKF_APPROX and LKF_ERROR with bad free"
+grep "Terminating" test_file > /dev/null 2>&1
+$assert_success
+
 printf "==================== TEST02.SH END ====================\n"
 test_type="test02.sh"
+rm test_file
 . shell_tests/finish.sh
