868ea2d6287197b74fca4762361659db03acf86a
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-28 02:42:19 -0400

    Modified alloc_with_flags_and_modify to use the new test_utils.h header

--- test/src/alloc_with_flags_and_modify.c
+++ test/src/alloc_with_flags_and_modify.c
@@ -3,14 +3,17 @@
 #include <unistd.h>
 
 #include "lkmalloc.h"
+#include "test_utils.h"
 
 /* size, malloc flags, free flags, buffer offset */
 int main(int argc, char *argv[]) {
 	if (argc != 5) return EXIT_FAILURE;
-	u_int size = atoi(argv[1]);
-	u_int malloc_flags = atoi(argv[2]);
-	u_int free_flags = atoi(argv[3]);
-	int buffer_offset = atoi(argv[4]);
+	u_int size, malloc_flags, free_flags;
+	int buffer_offset;
+	CONVERT_NUM(argv[1], &size);
+	CONVERT_NUM(argv[2], &malloc_flags);
+	CONVERT_NUM(argv[3], &free_flags);
+	CONVERT_NUM(argv[4], &buffer_offset);
 	printf("buffer offset %d\n", buffer_offset);
 	void *buf = NULL;
 	int ret = lkmalloc(size, &buf, malloc_flags);
