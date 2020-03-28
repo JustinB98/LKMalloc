e216cd76cf2724e00c370c7db786fbdf880aea81
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-28 04:04:22 -0400

    Suppressed output in test06.sh

--- test/shell_tests/ec_tests/test06.sh
+++ test/shell_tests/ec_tests/test06.sh
@@ -11,23 +11,23 @@ assert_malloc_flags="bin/assert_valid_lkmalloc_flags"
 echo "============ TEST06.SH START ==============="
 
 test_name="LKM_PROT_BEFORE and LKM_PROT_AFTER non page size allocation"
-$assert_malloc_flags 4095 0x18 0
+$assert_malloc_flags 4095 0x18 0 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_PROT_BEFORE and LKM_PROT_AFTER non page size allocation 2"
-$assert_malloc_flags 8193 0x18 0
+$assert_malloc_flags 8193 0x18 0 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_PROT_BEFORE and LKM_PROT_AFTER 1 page size"
-$assert_malloc_flags 4096 0x18 1
+$assert_malloc_flags 4096 0x18 1 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_PROT_BEFORE and LKM_PROT_AFTER 2 page size"
-$assert_malloc_flags 8192 0x18 1
+$assert_malloc_flags 8192 0x18 1 > /dev/null 2>&1
 $assert_success
 
 test_name="LKM_PROT_BEFORE and LKM_PROT_AFTER 3 page size"
-$assert_malloc_flags 16384 0x18 1
+$assert_malloc_flags 16384 0x18 1 > /dev/null 2>&1
 $assert_success
 
 printf "==================== TEST06.SH END ====================\n"
