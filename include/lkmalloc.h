8c9a5254cd510398f5668b4df4f4a6cd6ad97829
Justin Behrman <justin.behrman@stonybrook.edu>
2020-03-28 20:14:17 -0400

    Commented lkmalloc.h and fixed ifndef

--- include/lkmalloc.h
+++ include/lkmalloc.h
@@ -1,5 +1,5 @@
-#ifndef LK_LIB_H
-#define LK_LIB_H
+#ifndef LK_MALLOC_H
+#define LK_MALLOC_H
 
 #include <stdlib.h>
 
@@ -24,14 +24,43 @@
 #define LKR_DOUBLE_FREE 0x10
 #define LKR_APPROX 0x20
 
+/*
+ * Mallocs a specified amount of memory and fills pointer passed with that address
+ * @param size	Size to malloc
+ * @param ptr	Double pointer that pointers to the area to store the malloced pointer to
+ * @param flags	Flags specify options
+ * @return		0 if success
+ *				-EINVAL if error
+ */
 #define lkmalloc(size, ptr, flags) __lkmalloc_internal((size), (ptr), (flags), __FILE__, __func__, __LINE__)
+
+/*
+ * Frees pointer from lkmalloc
+ * @param ptr	Pointer that points to address to be freed
+ * @param flags	Flags to change the behavior of lkfree
+ * @return		0 for success
+ *				-EINVAL if invalid arugments
+ *				-ENOENT if pointer could not be found
+ */
 #define lkfree(ptr, flags) __lkfree_internal((ptr), (flags), __FILE__, __func__, __LINE__)
+
+/*
+ * Prints report to specified file descriptor
+ * @param fd	File descriptor to print report to
+ * @param flags	Options to change what gets printed
+ * @return		number of records printed if success (greater than or equal to 0)
+ *				-EINVAL for invalid arguments
+ * 				-errno if error (usually relating to printing)
+ */
 #define lkreport(fd, flags) __lkreport_internal((fd), (flags))
 
+/* INTERNAL CALL DO NOT INVOKE DIRECTLY */
 int __lkmalloc_internal(u_int size, void **ptr, u_int flags, char *file, const char *func, int line);
 
+/* INTERNAL CALL DO NOT INVOKE DIRECTLY */
 int __lkfree_internal(void **ptr, u_int flags, char *file, const char *func, int line);
 
+/* INTERNAL CALL DO NOT INVOKE DIRECTLY */
 int __lkreport_internal(int fd, u_int flags);
 
-#endif /* LK_LIB_H */
+#endif /* LK_MALLOC_H */
